aa5a1d363fae1791c83f66a83eb94ad38087ce0c
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-04-24 21:31:19 -0400

    write route for login and redirect

--- friends/src/component/Login/LogIn.jsx
+++ friends/src/component/Login/LogIn.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import './LogIn.scss';
 
 function LogIn() {
-  return (
-    <div className="LogIn">
-      <form>
-          <input type='text' name='username' placeholder='Enter your username'/>
-          <input type='password' name='password' placeholder='Enter your password'/>
-          <input type="submit"/>
-      </form>
+    return (
+        <div className="LogIn">
+            <h1>Log in to view content</h1>
+        <form>
+            <input type='text' name='username' placeholder='Enter your username'/>
+            <input type='password' name='password' placeholder='Enter your password'/>
+            <input type="submit"/>
+        </form>
     </div>
-  );
+    );
 }
 
 export default LogIn;
--- friends/src/container/App.jsx
+++ friends/src/container/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import LogIn from '../component/Login/LogIn';
+import { Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <LogIn/>
+      <Route path='/login' render={LogIn}/>
+      <Redirect from='/' to='/login'/>
     </div>
   );
 }
