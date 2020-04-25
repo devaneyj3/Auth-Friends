5b8b206beff30c3a04a03bdf843dbdeb563e05dd
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-04-24 21:23:12 -0400

    set up folder structure and make log in form

---
+++ friends/src/container/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import LogIn from '../component/Login/LogIn';
+
+function App() {
+  return (
+    <div className="App">
+      <LogIn/>
+    </div>
+  );
+}
+
+export default App;
