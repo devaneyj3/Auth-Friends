861f41f2ee8e7171c86a59c18425b168e004896d
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-04-26 23:17:07 -0400

    add style to friends component

--- friends/src/component/FriendList/FriendList.js
+++ friends/src/component/FriendList/FriendList.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import './FriendList.scss';
+import React from "react";
+import "./FriendList.scss";
 
-const FriendList = (props) => {
-    return (
-        <div key={props.id} className="FriendList">
-            <h1>{props.name}</h1>
-            <p>{props.age}</p>
-            <p>{props.email}</p>
-        </div>
-    );
-}
+const FriendList = props => {
+  return (
+    <div key={props.id} className="friend-card">
+      <h1>{props.name}</h1>
+      <p>{props.age}</p>
+      <p>{props.email}</p>
+      <button>Delete</button>
+    </div>
+  );
+};
 
 export default FriendList;
--- friends/src/component/Friends/Friends.js
+++ friends/src/component/Friends/Friends.js
@@ -1,32 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import './Friends.scss';
-import AxiosWithAuth from '../../Auth/AxiosWithAuth';
-import FriendList from '../FriendList/FriendList';
+import React, { useEffect, useState } from "react";
+import "./Friends.scss";
+import AxiosWithAuth from "../../Auth/AxiosWithAuth";
+import FriendList from "../FriendList/FriendList";
 
-const Friends = (props) => {
-    const [friends, setFriends ] = useState([])
+const Friends = props => {
+  const [friends, setFriends] = useState([]);
 
-    useEffect(() => {
-        AxiosWithAuth().get('http://localhost:5000/api/friends')
-            .then(res => setFriends(res.data))
-            .catch( err => console.error( err))
-    }, [])
-        return (
-        <div className="Friends">
-            <h1>Here is your friends list</h1>
-            {friends.map(friend => {
-                return (
-                    <FriendList
-                        id={friend.id}
-                        name={friend.name}
-                        age={friend.age}
-                        email={friend.email}
-                        />
-                )
-            })}
-            
-    </div>
-    );
-}
+  useEffect(() => {
+    AxiosWithAuth()
+      .get("http://localhost:5000/api/friends")
+      .then(res => setFriends(res.data))
+      .catch(err => console.error(err));
+  }, []);
+  return (
+    <>
+      <section className="friends-heading">
+        <h3>Wow! People like you</h3>
+      </section>
+      <div className="cards">
+        {friends.map(friend => {
+          return (
+            <FriendList
+              id={friend.id}
+              name={friend.name}
+              age={friend.age}
+              email={friend.email}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default Friends;
